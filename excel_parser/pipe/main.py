5708ec8ac20cd47c4d00fb84d781a533d4d667b2
sahilverma0696 <sahilverma0696@gmail.com>
2019-10-21 06:56:31 +0530

    Start with bot

--- excel_parser/pipe/main.py
+++ excel_parser/pipe/main.py
@@ -1,21 +1,46 @@
-# main execution script
-# major calling functions to be made
-
 from io_handles import *
 from df_functions import *
-from os import path
-import sys
 
 
-file = pd.ExcelFile(excel_file())
+if(check_arguments()==False):
+    sys.exit(0)
+
+if(check_all):
+    excel_file = pd.ExcelFile(excel_file())
+    yml_data = yml_file()
+    print(yml_data)
+else:
+    print("False")
+    sys.exit(0)
 
 
-number_of_sheets=len(file.sheet_names)
+number_of_sheets = len(excel_file.sheet_names)
 
+
+
+def base_df(excel_file,i):
+    df = pd.read_excel(excel_file,sheet_name=excel_file.sheet_names[i],header = None)
+
+    
+    #Producing clean dataframe in lowercase.
+
+    df =df_clean(df)
+    df =df.applymap(lambda s:s.lower() if type(s) == str else s)
+    #Creating the dataframe from origin
+    #try:
+    origin = coordinates(df,yml_data['origin'])
+    #except:
+    #    print("Origin not found")
+    #    exit(0)
+
+    df= df.iloc[origin[0]:,origin[1]:]
+    reset_index(df)
+
+    return df
 
 
 def execute(i):
-    df = base_df(file,i)
+    df = base_df(excel_file,i)
     
 
     ## TODO: seprate the normal df construct vs period data construct 
@@ -44,15 +69,8 @@ def execute(i):
 
 
     ##### EXPORTING DATA
-
-    if not path.exists(path.expanduser("~/Desktop/Spacemonk/output")):
-        makedirs(path.expanduser("~/Desktop")+"/Spacemonk/output")
-    if(yml_data['excel_copy']):
-        df_act.to_excel(path.expanduser("~/Desktop/Spacemonk/output/")+file.sheet_names[i]+".xlsx")
-    else:
-        pass
-    df_act.to_json(path.expanduser("~/Desktop/Spacemonk/output/")+file.sheet_names[i]+".json" ,orient = 'records')
-    print(file.sheet_names[i], " succesfull")
+    df_act.to_excel(excel_file.sheet_names[i]+".xlsx")
+    print(excel_file.sheet_names[i], " succesfull")
 
 
 
