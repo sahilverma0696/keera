8b56e466ec0ffcf2c85414e1bcdf4c641ee0f0c5
sahilverma0696 <sahilverma0696@gmail.com>
2019-10-22 06:59:27 +0530

    Bot update

--- excel_parser/bot/beta.py
+++ excel_parser/bot/beta.py
@@ -1,32 +1,24 @@
 import telepot 
 from telepot.loop import MessageLoop
 from time import sleep
-import os
-import random
+
 TOKEN = "970138714:AAE2zD2XT252iZ1ggiWiTMg9nzPh09TJmuM"
 bot = telepot.Bot(TOKEN)
 
-
-
-def photo_list():
-    photo_list =[]
-    for each in os.listdir('./photos'):
-        photo_list.append(open("./photos/"+each,"rb"))
-    return photo_list
-
-
-
-x=photo_list()    
-    
+response = bot.getUpdates()
+offset= response['update_id']
+print(offset)
+'''
+   
 def handle(msg):
     
     content_type, chat_type, chat_id = telepot.glance(msg)
     print(content_type,chat_id,chat_type)
     if content_type == "text":
-        bot.sendPhoto(chat_id,random.choice(x))
+        bot.sendMessage(chat_id,"Hello")
 MessageLoop(bot,handle).run_as_thread()
 
 print("Running")
 
 while(1):
-    sleep(10)
+    sleep(10)'''
---
+++ excel_parser/bot/bot_fun.py
@@ -0,0 +1,11 @@
+def greet(chat_id):
+     bot.sendMessage(chat_id,"Hello Welcome to Spacemonk")
+
+
+def latest_offset():
+    
+
+def first_message():
+
+
+def workorder()
