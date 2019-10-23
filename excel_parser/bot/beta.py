911024a87f741afe6dee037ae9d6cac62de32cbf
sahilverma0696 <sahilverma0696@gmail.com>
2019-10-23 06:44:04 +0530

    bot updates

--- excel_parser/bot/beta.py
+++ excel_parser/bot/beta.py
@@ -1,24 +1,30 @@
 import telepot 
 from telepot.loop import MessageLoop
 from time import sleep
-
+from pprint import pprint
+import bot_fun
 TOKEN = "970138714:AAE2zD2XT252iZ1ggiWiTMg9nzPh09TJmuM"
 bot = telepot.Bot(TOKEN)
 
-response = bot.getUpdates()
-offset= response['update_id']
-print(offset)
-'''
-   
+count =1
+# to recieve msg regularly
 def handle(msg):
-    
+    #pprint(msg)
+    global count 
     content_type, chat_type, chat_id = telepot.glance(msg)
-    print(content_type,chat_id,chat_type)
-    if content_type == "text":
-        bot.sendMessage(chat_id,"Hello")
+    text = msg['text'];from_name = msg['from']['first_name']
+    print(chat_id,content_type,from_name,text)
+    '''if(count ==1):
+        bot_fun.greet(bot,chat_id)
+    count = count + 1'''
+    #if(text == 'workorders'):
+    #    bot_fun.yml_questions(bot,chat_id)
+
+def handle1(msg):
+    bot.sendMessage(chat_id,"H")
 MessageLoop(bot,handle).run_as_thread()
 
 print("Running")
 
 while(1):
-    sleep(10)'''
+    sleep(10)
