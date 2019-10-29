cec9c9ae38e211959d75bf352502d082ede7581e
sahilverma0696 <sahilverma0696@gmail.com>
2019-10-29 07:29:12 +0530

    Updates in bot

--- excel_parser/bot/beta.py
+++ excel_parser/bot/beta.py
@@ -1,27 +1,49 @@
+#TODO: Make the bot export_json function by having the file
+
+
 import telepot 
 from telepot.loop import MessageLoop
 from time import sleep
 from pprint import pprint
-import bot_fun
+#from telegram import File
 TOKEN = "970138714:AAE2zD2XT252iZ1ggiWiTMg9nzPh09TJmuM"
 bot = telepot.Bot(TOKEN)
 
-count =1
-# to recieve msg regularly
-def handle(msg):
-    #pprint(msg)
-    global count 
-    content_type, chat_type, chat_id = telepot.glance(msg)
-    text = msg['text'];from_name = msg['from']['first_name']
-    print(chat_id,content_type,from_name,text)
-    '''if(count ==1):
-        bot_fun.greet(bot,chat_id)
-    count = count + 1'''
-    #if(text == 'workorders'):
-    #    bot_fun.yml_questions(bot,chat_id)
+def yml_questions(chat_id):
+    bot.sendMessage(chat_id,"Please answer the following questions.")
+#def gen_json():
 
-def handle1(msg):
-    bot.sendMessage(chat_id,"H")
+def text_handler(msg,chat_id): 
+    text = msg['text'];from_name = msg['from']['first_name']
+    print(chat_id,from_name,text)
+    if(text == 'Make workorder'):
+        bot.sendMessage(chat_id,"Sure, let's get started")
+        yml_questions(chat_id)
+    elif(text =="generate json"):
+        bot.sendMessage(chat_id,"Sure, please upload the std excel format")
+    else:
+        bot.sendMessage(chat_id,"Sorry I don't understand that.")
+    
+def file_handle(msg):
+    file_id = msg['document']['file_id']
+    print(bot.getFile(file_id))
+    '''download_file =File
+    download_file.file_id(file_id)
+    download_file.download(custom_path ='./')'''
+
+
+
+def handle(msg):
+    pprint(msg)
+    content_type, chat_type, chat_id = telepot.glance(msg)
+    if(content_type=='text'):
+        text_handler(msg,chat_id)
+    elif(content_type== 'document'):
+        file_handle(msg)
+
+    
+
+
 MessageLoop(bot,handle).run_as_thread()
 
 print("Running")
